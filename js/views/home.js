18a342b35d3ded64d6c5f554f01c07fdd9a2ef72
Cristobal Castilla <cristobal@aerstudio.com>
2012-12-03 09:02:58 +0100

    Added class container to mapukaApp div

--- js/views/home.js
+++ js/views/home.js
@@ -5,9 +5,10 @@ window.mapukaApp = window.mapukaApp || {};
 // HOME class
 mapukaApp.HomeView = Backbone.View.extend({
   id: "home", 
-  className: "container",
   // template: mapukaApp.tpl.templates['home'], 
-  events: {}, 
+  events: {
+    "click form a.submit": "onSubmitClick"
+  }, 
 
   initialize: function () {
     this.template = mapukaApp.tpl.templates['home'];
@@ -15,6 +16,14 @@ mapukaApp.HomeView = Backbone.View.extend({
 
   render: function () {
     this.$el.append( _.template(this.template) );
+    return this;
+  }, 
+
+  onSubmitClick: function (e) {
+    e.preventDefault();
+
+    var code = this.$('form input[name=code]').val();
+    mapukaApp.router.navigate('ficha/'+code);
     return this;
   }
 });
